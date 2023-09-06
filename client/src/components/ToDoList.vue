39ad84509705c47e490e94e3164f222aff86428f
owainow <owainosborne@tigerbay.uk.com>
2023-09-06 12:35:07 +0100

    Restructured divs

--- client/src/components/ToDoList.vue
+++ client/src/components/ToDoList.vue
@@ -10,8 +10,8 @@
       <ul class="todo-list">        
         <li v-for="todo in filteredTodos" class="todo" :key="todo.id" :class="{ completed: todo.completed, editing: todo == editedTodo }" 
           draggable="true" @dragstart="dragStart($event, todo)" @drop="dragDrop($event, todo)" @dragenter="dragEnter($event)" @dragleave="dragLeave($event)" @dragover.prevent>
-          <div class="view">
-            <div class="div1">
+           <div class="div1"> 
+            <div class="view">
               <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
               <input @change="completeTodo(todo)" class="toggle" type="checkbox" v-model="todo.completed" />
             </div>
@@ -25,8 +25,8 @@
       <ul class="todo-list">        
         <li v-for="todo in filteredTodos" class="todo" :key="todo.id" :class="{ completed: todo.completed, editing: todo == editedTodo }" 
           draggable="true" @dragstart="dragStart($event, todo)" @drop="dragDrop($event, todo)" @dragenter="dragEnter($event)" @dragleave="dragLeave($event)" @dragover.prevent>
-          <div class="view">
-            <div class="div2">
+          <div class="div2">
+            <div class="view">
               <label @dblclick="editTodo(duedate)">{{ todo.duedate }}</label>
               <button class="destroy" @click="removeTodo(todo)"></button>
             </div>
