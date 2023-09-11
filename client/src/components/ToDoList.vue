bfc86f1e60badaf51c4451c576533025a8aba88c
owainow <owainosborne@tigerbay.uk.com>
2023-09-11 13:38:01 +0100

    Testing duplicate complete checkboxes

--- client/src/components/ToDoList.vue
+++ client/src/components/ToDoList.vue
@@ -12,12 +12,13 @@
           draggable="true" @dragstart="dragStart($event, todo)" @drop="dragDrop($event, todo)" @dragenter="dragEnter($event)" @dragleave="dragLeave($event)" @dragover.prevent>
           <div class="view">
             <input @change="completeTodo(todo)" class="toggle" type="checkbox" v-model="todo.completed" />
+            <input @change="completeTodo(todo)" class="toggle" type="checkbox" v-model="todo.completed" />
             <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
             <button class="destroy" @click="removeTodo(todo)"></button>
           </div>
           <input class="edit" type="text" v-model="todo.title" v-todo-focus="todo == editedTodo" @blur="doneEdit(todo)"
             @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" />
-          <input @change="inprogressTodo(todo)" class="toggle" type="checkbox" v-model="todo.inprogress" />
+
         </li>
       </ul>
     </section>
