231f8598e163f28236cdbcbcf18d4b645a86e5ef
owainow <owainosborne@tigerbay.uk.com>
2023-09-11 15:40:01 +0100

    div

--- client/src/components/ToDoList.vue
+++ client/src/components/ToDoList.vue
@@ -10,15 +10,15 @@
       <ul class="todo-list">        
         <li v-for="todo in filteredTodos" class="todo" :key="todo.id" :class="{ inprogress: todo.inprogress, completed: todo.completed, editing: todo == editedTodo }" 
           draggable="true" @dragstart="dragStart($event, todo)" @drop="dragDrop($event, todo)" @dragenter="dragEnter($event)" @dragleave="dragLeave($event)" @dragover.prevent>
-          <div class="view">
+          <div class="view" id="1">
             <input @change="completeTodo(todo)" class="toggle" type="checkbox" v-model="todo.completed" />
             <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
             <button class="destroy" @click="removeTodo(todo)"></button>
-            
+                  <div class="float" id="2">
+                      <input @change="inprogressTodo(todo)" class="toggle" type="checkbox" v-model="todo.inprogress" />
+                  </div>
           </div>
-          <div class="float">
-          <input @change="inprogressTodo(todo)" class="toggle" type="checkbox" v-model="todo.inprogress" />
-          </div>
+
           <input class="edit" type="text" v-model="todo.title" v-todo-focus="todo == editedTodo" @blur="doneEdit(todo)"
             @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" />
         </li>
