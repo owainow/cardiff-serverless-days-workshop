68d70708c1472849a542b5d4e91fb393498163c5
owainow <owainosborne@tigerbay.uk.com>
2023-09-12 09:56:24 +0100

    Updated div to togggle 2

--- client/src/components/ToDoList.vue
+++ client/src/components/ToDoList.vue
@@ -12,7 +12,7 @@
           draggable="true" @dragstart="dragStart($event, todo)" @drop="dragDrop($event, todo)" @dragenter="dragEnter($event)" @dragleave="dragLeave($event)" @dragover.prevent>
           <div class="view" id="1">
             <input @change="completeTodo(todo)" class="toggle" type="checkbox" v-model="todo.completed" />
-            <input @change="inprogressTodo(todo)" class="toggle" type="checkbox" v-model="todo.inprogress" />
+            <input @change="inprogressTodo(todo)" class="toggle2" type="checkbox" v-model="todo.inprogress" />
             <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
             <button class="destroy" @click="removeTodo(todo)"></button>
           </div>
