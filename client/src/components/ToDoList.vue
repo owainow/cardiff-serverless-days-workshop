cede7bf6b2c230b8aaa2c360438011f67a8868c7
owainow <owainosborne@tigerbay.uk.com>
2023-09-13 15:07:30 +0100

    Revert

--- client/src/components/ToDoList.vue
+++ client/src/components/ToDoList.vue
@@ -14,7 +14,8 @@
             <input @change="completeTodo(todo)" class="toggle" type="checkbox" v-model="todo.completed" /> 
             <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
             <button class="destroy" @click="removeTodo(todo)"></button>
-            <input @change="inprogressTodo(todo)" type='checkbox' name='inprog' v-model="todo.inprogress" id="inprog"/><label for="inprog"></label>  
+            <input id="inprogcheck" @change="inprogressTodo(todo)" class="inprogtoggle" type="checkbox" v-model="todo.inprogress"><label for="inprogcheck" class="inprogtoggle"></label> 
+            
           </div>
 
           <input class="edit" type="text" v-model="todo.title" v-todo-focus="todo == editedTodo" @blur="doneEdit(todo)"
