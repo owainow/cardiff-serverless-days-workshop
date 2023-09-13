29156ad1e57cc6bfad80fdab94302b67d8ef6c2d
owainow <owainosborne@tigerbay.uk.com>
2023-09-13 14:51:30 +0100

    Returned label

--- client/src/components/ToDoList.vue
+++ client/src/components/ToDoList.vue
@@ -15,7 +15,6 @@
             <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
             <button class="destroy" @click="removeTodo(todo)"></button>
             <input id="inprogcheck" @change="inprogressTodo(todo)" class="inprogtoggle" type="checkbox" v-model="todo.inprogress"/><label for="inprogcheck" class="inprogtoggle"></label> 
-            
           </div>
 
           <input class="edit" type="text" v-model="todo.title" v-todo-focus="todo == editedTodo" @blur="doneEdit(todo)"
